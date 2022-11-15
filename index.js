575765971f8e4508aecfde02f9d43dfd008fc767
KylePrograms <99099360+KylePrograms2by2@users.noreply.github.com>
2022-11-15 19:20:39 +0000

    Removed junk

--- index.js
+++ index.js
@@ -9,14 +9,4 @@ require('./utils/structure/registery')(client);
 require('./utils/handlers/commands')(client);
 require('./utils/handlers/events')(client);
 
-client.on('message', async(message) => {
-    message.channel.messages.fetch()
-require('./utils/handlers/handler')(client, message)
-});
-
-client.on('messageUpdate', (o, message) => {
-require('./utils/handlers/editHandles')(client, message);
-})
-
-
 client.login(process.env.TOKEN);
